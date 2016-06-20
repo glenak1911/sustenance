74aea49ed89f15a3ca55d36c058abad11a7ce2b6
Glen Knight <glenak1911@gmail.com>
2016-06-19 22:40:04 -0400

    added input and ingredient list specification

--- src/sustenanceSrc/Main.java
+++ src/sustenanceSrc/Main.java
@@ -5,13 +5,26 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import java.io.*;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+
 import org.json.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {	
+		Scanner reader = new Scanner(System.in);
+		System.out.println("What ingredients do you want to use Boss?");
+		String ingredients = reader.next();
+		ingredients = ingredients.replace(",", "%2C");
+		findByIngredients(ingredients);
+
+		
+	}
+	
+	public static void findByIngredients(String ingredients){
 		try {
-			HttpResponse<JsonNode> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=5&ranking=1")
+			HttpResponse<JsonNode> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients="+ingredients+"&limitLicense=false&number=5&ranking=1")
 					.header("X-Mashape-Key", "yyss38ddAYmshKSQkToaZnu7Qd9Yp1OGXDfjsnpDe333h6xtq5")
 					.header("Accept", "application/json")
 					.asJson();
@@ -22,8 +35,7 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 	}
-
 }
+
+
